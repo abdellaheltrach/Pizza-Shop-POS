3647c16d055ef83b6cc9cd84d3f0f1e483738145
abdellaheltrach <abdellah.eltrach@gmail.com>
2024-06-27 00:58:27 +0200

    Adding index

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,7 +61,7 @@
             this.button1.Location = new System.Drawing.Point(292, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(212, 57);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 5;
             this.button1.Text = "Copy";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
@@ -72,7 +72,8 @@
             this.button2.Location = new System.Drawing.Point(292, 315);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(212, 57);
-            this.button2.TabIndex = 3;
+            this.button2.TabIndex = 4;
+            this.button2.TabStop = false;
             this.button2.Text = "Copy on mouse enter";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
@@ -83,7 +84,7 @@
             this.button3.Location = new System.Drawing.Point(292, 238);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(212, 57);
-            this.button3.TabIndex = 4;
+            this.button3.TabIndex = 3;
             this.button3.Text = "enable text box1";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
@@ -94,7 +95,7 @@
             this.button4.Location = new System.Drawing.Point(292, 161);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(212, 57);
-            this.button4.TabIndex = 5;
+            this.button4.TabIndex = 2;
             this.button4.Text = "Disable text box1";
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
@@ -119,7 +120,7 @@
             this.button5.Location = new System.Drawing.Point(292, 469);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(213, 74);
-            this.button5.TabIndex = 7;
+            this.button5.TabIndex = 6;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             // 
@@ -137,7 +138,7 @@
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "KOKO";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
