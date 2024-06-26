7658fdae8ce7627ee72bd9e5f21ff517d1adadc5
abdellaheltrach <abdellah.eltrach@gmail.com>
2024-06-26 17:03:37 +0200

    Learn windows forms properties
    Adding textbox's and labels and buttons and adjust their properties

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,15 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(83, 103);
+            this.textBox1.Location = new System.Drawing.Point(78, 124);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(265, 22);
             this.textBox1.TabIndex = 0;
@@ -44,7 +48,8 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(453, 103);
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(448, 124);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(265, 22);
             this.textBox2.TabIndex = 1;
@@ -53,7 +58,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.LightGreen;
-            this.button1.Location = new System.Drawing.Point(308, 190);
+            this.button1.Location = new System.Drawing.Point(292, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(212, 57);
             this.button1.TabIndex = 2;
@@ -63,20 +68,70 @@
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Salmon;
-            this.button2.Location = new System.Drawing.Point(308, 305);
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Location = new System.Drawing.Point(292, 315);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(212, 57);
             this.button2.TabIndex = 3;
-            this.button2.Text = "Delete";
+            this.button2.Text = "Copy on mouse enter";
             this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGreen;
+            this.button3.Location = new System.Drawing.Point(292, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 57);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "enable text box1";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGreen;
+            this.button4.Location = new System.Drawing.Point(292, 161);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(212, 57);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Disable text box1";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 37);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "this is my first label ever";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 3;
+            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(292, 469);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(213, 74);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(834, 558);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
@@ -95,6 +150,10 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,7 @@ namespace Pizza_Shop_POS
         public Form1()
         {
             InitializeComponent();
+          
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -24,7 +26,8 @@ namespace Pizza_Shop_POS
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            
+            textBox2.Text = textBox1.Text;
+            label1.Text=textBox1.Text; 
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -36,10 +39,24 @@ namespace Pizza_Shop_POS
             textBox2.Text = textBox1.Text;
         }
 
-        private void button2_Click_1(object sender, EventArgs e)
+        private void button2_MouseEnter(object sender, EventArgs e)
         {
-            textBox2.Text = textBox1.Text="";
+            textBox2.Text = textBox1.Text;
         }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
